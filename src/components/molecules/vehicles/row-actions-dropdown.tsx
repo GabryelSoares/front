7175433ab93c72da8f407c8bfc74e782797f13b2
gabryelsoares <gabryelfhsoares@gmail.com>
2023-12-23 09:39:53 -0300

    refactor: RowActionsDropdown

--- src/components/molecules/vehicles/row-actions-dropdown.tsx
+++ src/components/molecules/vehicles/row-actions-dropdown.tsx
@@ -30,28 +30,31 @@ export function RowActionsDropdown({ vehicle }: Props) {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline">
+        <Button variant="destructive" className="hover:bg-gray-100 hover:border hover:border-gray-300 transition-transform duration-200 hover:cursor-pointer">
           <DotsVerticalIcon />
         </Button>
       </DropdownMenuTrigger>
-      <DropdownMenuContent className="w-56 bg-white">
+      <DropdownMenuContent className="w-56 bg-white p-0 m-0">
         <DropdownMenuLabel>Ações</DropdownMenuLabel>
         <DropdownMenuSeparator />
-        <DropdownMenuItem onClick={() => {
-          console.log('Visualizar:: ', vehicle)
+        <DropdownMenuItem className="p-0" onClick={() => {
           setShowViewVehicleModal(true)
           setSelectedVehicles([vehicle])
-        }}>Visualizar</DropdownMenuItem>
-        <DropdownMenuItem onClick={() => {
-          console.log('Alterar:: ', vehicle)
+        }}>
+          <span className="hover:bg-gray-100 p-2 w-full h-full transition-transform duration-200 hover:cursor-pointer">Visualizar</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem className="p-0" onClick={() => {
           setShowUpdateVehicleModal(true)
           setSelectedVehicles([vehicle])
-        }}>Alterar</DropdownMenuItem>
-        <DropdownMenuItem onClick={() => {
-          console.log('Apagar:: ', vehicle)
+        }}>
+          <span className="hover:bg-gray-100 p-2 w-full h-full transition-transform duration-200 hover:cursor-pointer">Alterar</span>
+        </DropdownMenuItem>
+        <DropdownMenuItem className="p-0" onClick={() => {
           setShowDeleteVehicleModal(true)
           setSelectedVehicles([vehicle])
-        }}>Apagar</DropdownMenuItem>
+        }}>
+          <span className="hover:bg-gray-100 p-2 w-full h-full transition-transform duration-200 hover:cursor-pointer">Apagar</span>
+        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   )
